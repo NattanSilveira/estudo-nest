d6981b08217cbe5d040dba5591a2bb30490264f4
Nattan Sousa <nattan_lion@msn.com>
2022-06-13 23:02:34 -0300

    enviando parametros na requisição get

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Param } from "@nestjs/common";
 
 @Controller('courses')
 export class CoursesController {
   @Get('list')
   findAll() {
     return 'listagem de cursos';
+  }
+  // @Get(':id') //: indica o parametro recebido
+  // findOne(@Param() params) {
+  //   return `Curso ${params.id}`;
+  // }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    //indicando o parametro especifico a ser capturado e tipando ele
+    return `Curso ${id}`;
   }
 }
